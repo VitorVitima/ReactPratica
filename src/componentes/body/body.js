4681507020af58414e3cc39e3875ad9f6e013eae
VitorVitima <yukkiegg@gmail.com>
2023-01-16 21:43:26 -0300

    sla

--- src/componentes/body/body.js
+++ src/componentes/body/body.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './css/style.css'
 
 function sla(props) {
-    console.log(props)
+    console.log(props);
+    const number1 = 10;
+    const number2 = 5;
     return(
         <>
             <p>
                 O {props.nome} é muito {props.cara}.
+            </p>
+            <p>
+                {`A soma de ${number1} + ${number2} é igual a ${props.somar(number1, number2)}`}
+            </p>
+            <p>
+                {props.timeSeconds()}
             </p>
         </>
     )
