5f6009bdde3b07e24e3cc295a060965a4d397c17
VitorVitima <yukkiegg@gmail.com>
2023-01-29 11:48:15 -0300

    finalizado

--- src/pratica/calculadora/Buttons.js
+++ src/pratica/calculadora/Buttons.js
@@ -1,32 +1,42 @@
 import React from 'react';
 function AllButtons(props){
     const acClear = () => {
-        props.setS(0)
+        props.setS(0);
     }
-    const contatenar = (e) => {
-        return props.setS(props.register + e.target.value) //nao ta funcionando,  parei aqui
+    const concatenar = e => {
+        if(e.target.getAttribute('class') == null){
+            props.setVal(false);
+            console.log(props.val);           
+            return props.setS(props.s + e.target.value);
+        } else if (props.val == false){
+            props.setVal(true);
+            return props.setS(props.s + e.target.value);
+        }
     }
+    const resultado = () => {
+        return props.setS(eval(props.s));
+    }   
     return(
         <>
             <input id='ac' onClick={()=>acClear()} type='button' value='AC'/>
-            <input onClick={(e)=>contatenar(e)} type='button' value='('/>
-            <input type='button' value=')'/>
-            <input type='button' value='/'/>
-            <input type='button' value='7'/>
-            <input type='button' value='8'/>
-            <input type='button' value='9'/>
-            <input type='button' value='*'/>
-            <input type='button' value='4'/>
-            <input type='button' value='5'/>
-            <input type='button' value='6'/>
-            <input type='button' value='-'/>
-            <input type='button' value='1'/>
-            <input type='button' value='2'/>
-            <input type='button' value='3'/>
-            <input type='button' value='+'/>
-            <input type='button' value='0'/>
-            <input type='button' value='.'/>
-            <input id='iqual' type='button' value='='/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='('/>
+            <input onClick={(e)=>concatenar(e)} type='button' value=')'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='/' className='operador' />
+            <input onClick={(e)=>concatenar(e)} type='button' value='7'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='8'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='9'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='*' className='operador' />
+            <input onClick={(e)=>concatenar(e)} type='button' value='4'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='5'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='6'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='-' className='operador' />
+            <input onClick={(e)=>concatenar(e)} type='button' value='1'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='2'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='3'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='+' className='operador' />
+            <input onClick={(e)=>concatenar(e)} type='button' value='0'/>
+            <input onClick={(e)=>concatenar(e)} type='button' value='.' className='operador' />
+            <input onClick={()=>resultado()} id='iqual' type='button' value='='/>
         </>
     )   
 }
--- src/pratica/calculadora/Calc.js
+++ src/pratica/calculadora/Calc.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
 import Res from './Register.js';
 import Button from './Buttons.js';
+import './style/calcStyle.css';
 function Calculadora (){
-    const [register, setRegister] = useState('12')
+    const [register, setRegister] = useState('12');
+    const [validador, setVal] = useState(false);
     return(
-        <>
+        <div id='calcTag'>
             <div id='resultadoDiv'>
                 <Res num={register}></Res>
             </div>
             <div id='buttonsDiv'>
-                <Button setS={setRegister} s={register}></Button>
+                <Button setS={setRegister} s={register} val={validador} setVal={setVal}></Button>
             </div>
-        </>
+        </div>
     )
 }
 export default Calculadora;
