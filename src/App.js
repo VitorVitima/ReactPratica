86805bf29ae0989095371f81ffe65e9b5a0c691a
VitorVitima <yukkiegg@gmail.com>
2023-01-31 15:10:01 -0300

    save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Body from './componentes/body/body.js';
-import Header from './componentes/header/header.js';
-import Calc from './pratica/calculadora/Calc.js';
-import CalcImc from './pratica/calc imc/Imc.js';
-import JogoDaVelha from './pratica/jogoDaVelha/jogoDaVelha.js';
+import API01 from './apiPratica/test01/Test01ComAPI.js';
 function App() {
   return (
     <>
-      <Calc></Calc>
+      <API01></API01>
     </>
   );
 }
---
+++ src/apiPratica/test01/Test01ComAPI.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from 'axios';
+class Dados extends React.Component{
+    state = {
+        dados: []
+    }
+    componentDidMount(){
+        axios.get('https://MyFirstAPI.ai-se-fosse-o-p.repl.co')
+            .then(res=>{
+                this.setState({dados: res.data})
+            })
+    }
+    render(){
+        return(
+            <ul>  
+                {
+                    this.state.dados.map((el)=>{
+                        return (
+                                <li key={el.id}>
+                                    <strong>{el.nome}</strong> tem <strong>{el.idade}</strong> e mora em <strong>{el.estado}</strong>
+                                </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+export default Dados;
